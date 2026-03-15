1f694295aa26f45b9289f4824c5c33a90ac3e74b
SoumyadipYT-OSS <SoumyadipYT-OSS@users.noreply.github.com>
2026-03-15 17:20:20 +0530

    Add SQL scripts to create BikeStores database and schemas

---
+++ _03_SampleDatabase/SQLQuery2.sql
@@ -0,0 +1,10 @@
+USE BikeStores;
+GO
+
+-- create schema sales of BikeStores database 
+CREATE SCHEMA sales;
+GO
+
+-- create schema production of BikeStores database 
+CREATE SCHEMA production; 
+GO
